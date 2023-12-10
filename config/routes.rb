5e75b0d3bad2ec64b61873abeff7f85d729d18ba
Alhassan Osman <oalhassan847@gmail.com>
2023-12-10 02:19:06 +0000

    Fix minor errors

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'static#index'
-  get '/api/random_greeting', to: 'greetings#random'
+  get '/random_greeting', to: 'greetings#random'
 end
